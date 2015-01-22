2c65fca80a7237d278be3af5bf4823cc479fa3de
dhinesh <dhineshwiz@gmail.com>
2015-01-22 23:35:12 +0530

    my first commit 1

---
+++ src/com/projection/calculation/sample_calculate.java
@@ -0,0 +1,13 @@
+package com.projection.calculation;
+
+public class sample_calculate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("this is dhinesh");
+
+	}
+
+}
